2f3d54d86b38d0e5dc7ba25c216bbb19cebb4718
Saman Salimzade <113477417+samansmd94@users.noreply.github.com>
2025-09-07 11:14:52 -0700

    add signup and login

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 # Create your tests here.
+
+class HomePageViewTest(TestCase):
+    def test_home_page_view_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_view_text(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+class AboutPageViewTest(TestCase):
+    def test_aboutus_page_view_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_view_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_view_text(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'About Us')
