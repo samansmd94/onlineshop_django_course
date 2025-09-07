976f547cd0ac923d728f3986e82420953ad0e886
Saman Salimzade <113477417+samansmd94@users.noreply.github.com>
2025-09-07 12:23:35 -0700

    add crispy forms with bootstrap

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap4',
+
     'accounts',
     'pages',
 ]
@@ -135,3 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms config
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
--- pages/tests.py
+++ pages/tests.py
@@ -12,9 +12,13 @@ class HomePageViewTest(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_view_text(self):
+    def test_home_page_view_content(self):
         response = self.client.get(reverse('home'))
         self.assertContains(response, 'Home Page')
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
 
 class AboutPageViewTest(TestCase):
     def test_aboutus_page_view_url(self):
@@ -25,6 +29,36 @@ class AboutPageViewTest(TestCase):
         response = self.client.get(reverse('aboutus'))
         self.assertEqual(response.status_code, 200)
 
-    def test_aboutus_page_view_text(self):
+    def test_aboutus_page_view_content(self):
         response = self.client.get(reverse('aboutus'))
         self.assertContains(response, 'About Us')
+
+    def test_aboutus_page_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
+
+class SignupPageViewTest(TestCase):
+    def test_signup_page_view_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_view_template_used(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_page_view_content(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Sign Up')
+
+class LoginPageViewTest(TestCase):
+    def test_login_page_view_url_by_name(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_view_template_used(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_login_page_view_content(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, 'Log In')
